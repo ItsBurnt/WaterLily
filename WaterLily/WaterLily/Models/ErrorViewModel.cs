253bec7ed922edc8e0d9c58f40534a8d589c7fb4
Brent Schleper <bschleper@mmascend.com>
2023-11-29 22:11:51 -0500

    Init

---
+++ WaterLily/WaterLily/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WaterLily.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
